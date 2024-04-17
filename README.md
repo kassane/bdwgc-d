cc03d2658afea0b2b718a8ab9103ea92a5141acf
Matheus Catarino <matheus-catarino@hotmail.com>
2024-04-17 14:21:57 -0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bdwgc-d
-D library for interfacing with libgc (bdwgc)
+
+D library for interfacing with libgc (bdwgc) using [`importC`](https://dlang.org/spec/importc.html)
+
+### Compiler Support
+
+`bdwgc-d` should be compatible with multiple D compilers including `gdc`, `ldc2`, and `dmd`. When you use `dub` to build your project, it should automatically detect the compiler you have installed and use it to compile your code.
---
+++ src/bdwgc.d
@@ -0,0 +1,36 @@
+module bdwgc;
+public import gc;
+
+@("GC initialization")
+@system unittest
+{
+    GC_init();
+
+    for (auto i = 0; i < 1024; i++)
+    {
+        auto mem = cast(size_t*) GC_malloc(size_t.sizeof * 4);
+
+        *(mem + 0) = 0;
+        *(mem + 1) = 1;
+        *(mem + 2) = 2;
+        *(mem + 3) = 3;
+
+        assert(*(mem++) == 0);
+        assert(*(mem++) == 1);
+        assert(*(mem++) == 2);
+        assert(*(mem++) == 3);
+    }
+}
+
+@("GC incremental")
+@system unittest
+{
+    GC_init();
+    GC_enable_incremental();
+
+    for (auto i = 0; i < 1024; i++)
+    {
+        auto mem = GC_malloc(size_t.sizeof * 4);
+        GC_gcollect();
+    }
+}
